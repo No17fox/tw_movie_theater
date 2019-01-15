585abf8e65419f33a906e7169d03697eb3169850
KylinDC <inlovingdays@gmail.com>
2019-01-16 00:04:33 +0800

    Add the feature of get movie detail information

--- js/service.js
+++ js/service.js
@@ -19,3 +19,23 @@ function getMovieDiscuss(movieId, discussType, callback, count = 2, start = 0) {
     }
   });
 }
+
+function getMovieDetailInfo(movieId, callback) {
+  $.ajax({
+    type: "get",
+    url: `https://api.douban.com/v2/movie/subject/${movieId}`,
+    dataType: "jsonp",
+    jsonp: "callback",
+    data: {
+      apikey: "0b2bdeda43b5688921839c8ecb20399b",
+      client: "",
+      udid: ""
+    },
+    success: function(json) {
+      callback(json);
+    },
+    error: function() {
+      console.log(`failed to get movie detail information`);
+    }
+  });
+}
